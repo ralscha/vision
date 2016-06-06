dfff894e1a5a2e02fc4be5322ad2c03d2cbec64f
Ralph Schaer <ralphschaer@gmail.com>
2016-06-06 07:41:42 +0200

    Simple app that takes pictures and uploads them to the demo site

---
+++ app/app/pages/home/home.ts
@@ -0,0 +1,64 @@
+import {Page, NavController, Loading, Toast} from 'ionic-angular';
+import {Camera, Transfer} from 'ionic-native';
+
+@Page({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  private imageSrc: string;
+  private imageUploaded: boolean = true;
+  private loading: Loading;
+
+  constructor(private nav: NavController) {
+    this.loading = Loading.create({
+      content: "Uploading picture..."
+    });
+  }
+
+  upload() {
+    const fileTransfer = new Transfer();
+
+    this.nav.present(this.loading);
+    fileTransfer.upload(this.imageSrc, "https://demo.rasc.ch/vision/pictureupload")
+      .then(this.uploadSuccessful.bind(this))
+      .catch(this.uploadFailed.bind(this));
+  }
+
+  uploadSuccessful() {
+    this.imageUploaded = true;
+    this.loading.dismiss();
+
+    const toast = Toast.create({
+      message: 'Successfully uploaded',
+      duration: 3000
+    });
+
+    this.nav.present(toast);
+    Camera.cleanup();
+  }
+
+  uploadFailed() {
+    this.loading.dismiss();
+    const toast = Toast.create({
+      message: 'Upload failed',
+      duration: 3000
+    });
+
+    this.nav.present(toast);
+    Camera.cleanup();
+  }
+
+  takePicture() {
+    Camera.getPicture({
+      correctOrientation: true,
+      destinationType: Camera.DestinationType.FILE_URI,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then(imageFileUrl => {
+      this.imageSrc = imageFileUrl;
+      this.imageUploaded = false;
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+}
