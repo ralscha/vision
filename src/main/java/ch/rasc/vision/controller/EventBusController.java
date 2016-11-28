d5bdde176dec1f7b09502d8348f25d67770914e2
Ralph Schaer <ralphschaer@gmail.com>
2016-11-28 13:39:38 +0100

    Upgrade to extjs 6.2.1

--- src/main/java/ch/rasc/vision/controller/EventBusController.java
+++ src/main/java/ch/rasc/vision/controller/EventBusController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import ch.rasc.sse.eventbus.SseEvent;
 import ch.rasc.sse.eventbus.SseEventBus;
 
 @RestController
